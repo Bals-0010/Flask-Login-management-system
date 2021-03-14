12f72850db4529fa6e2e2ce8a04642227028bb67
Bals-0010 <bala.bhar920@gmail.com>
2021-03-14 15:24:51 +0530

    added screenshots

--- forms.py
+++ forms.py
@@ -1,14 +1,8 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, TextField, SubmitField
 from wtforms.validators import DataRequired, Length
-# from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 
 class LoginForm(FlaskForm):
     username = StringField('Username',validators=[DataRequired(), Length(min=4,max=20)])
     password = PasswordField('Password',validators=[DataRequired(), Length(min=4,max=24)])
     submit = SubmitField(' Login ')
-
-class test(FlaskForm):
-    firstname = StringField('Firstname',validators=[DataRequired(), Length(min=4,max=24)])
-    lastname = StringField('Lastname',validators=[DataRequired(), Length(min=4,max=24)])
-    submit = SubmitField('Save to DB')
--- main.py
+++ main.py
@@ -2,14 +2,12 @@ from flask import Flask, redirect, url_for, render_template, flash, request
 from forms import LoginForm
 from flask_login import login_user, current_user, logout_user, login_required
 from flask_sqlalchemy import SQLAlchemy
-from flask_bcrypt import Bcrypt
 from flask_login import LoginManager, UserMixin
 
 app = Flask(__name__)
 app.config['SECRET_KEY']='a43a3d1d4042a1a2291284552fd3f8ee'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///save.db'
 db = SQLAlchemy(app)
-bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
